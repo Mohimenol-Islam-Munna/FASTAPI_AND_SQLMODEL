11b578413070cb4163ef2d17287c2bc298fc9820
unknown <munna.cse.pust@gmail.com>
2024-01-05 12:31:05 +0600

    fastapi start

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-from typing import Union
 from fastapi import FastAPI
 
-app = FastAPI()
+# create  fastapi instance 
+app = FastAPI(debug=True)
 
 @app.get("/")
-def read_root():
-    pyDict = {"id": 1, "name": "munna", "campus": "PUST", "subject": "CSE"}
-    name = pyDict["name"]
-    subject = pyDict.get("campus")
-    pyDict["session"] = "2015-16"
-    pyDict.pop("subject")
-    pyDict.popitem()
-    pyDict.clear()
-
-    return pyDict
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def home():
+    return "This is home route of our application"
